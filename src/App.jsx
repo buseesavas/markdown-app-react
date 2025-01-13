99d5512ec1d6b24329136d77516d54e8dd45fc5a
[Buse Savaş] <busenursavas6@gmail.com>
2025-01-13 15:29:47 +0300

    bir default belge kaydettim

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,64 @@ import { LightModeSvg, DarkModeSvg } from "./Svg.jsx";
 
 function App() {
   const savedDocuments = JSON.parse(localStorage.getItem("documents")) || [];
+
+  const initialDocuments =
+    savedDocuments.length === 0
+      ? [
+          {
+            id: "1",
+            name: "welcome.md",
+            content: `
+# Welcome to Markdown
+
+Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents.
+
+## How to use this?
+
+1. Write markdown in the markdown editor window
+2. See the rendered markdown in the preview window
+
+### Features
+
+- Create headings, paragraphs, links, blockquotes, inline-code, code blocks, and lists
+- Name and save the document to access again later
+- Choose between Light or Dark mode depending on your preference
+
+> This is an example of a blockquote. If you would like to learn more about markdown syntax, you can visit this [markdown cheatsheet](https://www.markdownguide.org/cheat-sheet/).
+
+#### Headings
+
+To create a heading, add the hash sign (#) before the heading. The number of number signs you use should correspond to the heading level. You'll see in this guide that we've used all six heading levels (not necessarily in the correct way you should use headings!) to illustrate how they should look.
+
+##### Lists
+
+You can see examples of ordered and unordered lists above.
+
+###### Code Blocks
+
+This markdown editor allows for inline-code snippets, like this: \`<p>I'm inline</p>\`. It also allows for larger code blocks like this:
+
+\`\`\`
+<main>
+  <h1>This is a larger code block</h1>
+</main>
+\`\`\`
+      `,
+            createdAt: new Date().toLocaleString(),
+          },
+        ]
+      : savedDocuments;
+
+  const [documents, setDocuments] = useState(initialDocuments);
+
+  const saveDocuments = (newDocuments) => {
+    localStorage.setItem("documents", JSON.stringify(newDocuments));
+    setDocuments(newDocuments);
+  };
   const [darkMode, setDarkMode] = useState(
     localStorage.getItem("darkMode") === "true" ? true : false
   );
-  const [documents, setDocuments] = useState(savedDocuments);
+
   const [selectedDocument, setSelectedDocument] = useState(null);
   const [previewContent, setPreviewContent] = useState("");
   const [textAreaContent, setTextAreaContent] = useState("");
@@ -33,20 +87,21 @@ function App() {
   }
 
   function handleDelete() {
-    const updatedDocuments = documents.filter((doc) => doc.content !== previewContent); // Belgeyi içerik üzerinden filtreliyoruz
+    const updatedDocuments = documents.filter(
+      (doc) => doc.content !== previewContent
+    ); // Belgeyi içerik üzerinden filtreliyoruz
     setDocuments(updatedDocuments);
     setSelectedDocument(null);
     localStorage.setItem("documents", JSON.stringify(updatedDocuments));
-  
-  
+
     if (previewContent) {
       setTextAreaContent("");
       setPreviewContent("");
     }
-  
-    dialogRef.current.close(); 
+
+    dialogRef.current.close();
   }
-  
+
   function handleNewDocBtn() {
     setTextAreaContent("");
     setPreviewContent("");
@@ -165,15 +220,15 @@ function App() {
           </ul>
         </div>
         <label className="theme-switch">
-            <LightModeSvg className={!darkMode ? "icon-light" : "icon-dark"} />
-            <input
-              className="switch"
-              type="checkbox"
-              checked={darkMode}
-              onChange={changeDarkMode}
-            />
-            <DarkModeSvg className={darkMode ? "icon-light" : "icon-dark"} />
-          </label>
+          <LightModeSvg className={!darkMode ? "icon-light" : "icon-dark"} />
+          <input
+            className="switch"
+            type="checkbox"
+            checked={darkMode}
+            onChange={changeDarkMode}
+          />
+          <DarkModeSvg className={darkMode ? "icon-light" : "icon-dark"} />
+        </label>
       </div>
       <DeleteDialog dialogRef={dialogRef} handleDelete={handleDelete} />
     </div>
@@ -188,21 +243,24 @@ function DeleteDialog({ dialogRef, handleDelete }) {
   };
   return (
     <>
-    <dialog ref={dialogRef} className="delete-dialog" onClick={(e) => handleDialogClick(e)}>
-      <div className="dialog-container">
-        <h3>Delete this document?</h3>
-        <p>Are you sure you want to delete the document and its contents? This action cannot be reversed.</p>
-        <button className="delete-dialog-btn" onClick={handleDelete}>
-          Confirm & Delete
-        </button>
-      </div>
-    </dialog>
+      <dialog
+        ref={dialogRef}
+        className="delete-dialog"
+        onClick={(e) => handleDialogClick(e)}
+      >
+        <div className="dialog-container">
+          <h3>Delete this document?</h3>
+          <p>
+            Are you sure you want to delete the document and its contents? This
+            action cannot be reversed.
+          </p>
+          <button className="delete-dialog-btn" onClick={handleDelete}>
+            Confirm & Delete
+          </button>
+        </div>
+      </dialog>
     </>
   );
 }
 
-
-
 export default App;
-
-
